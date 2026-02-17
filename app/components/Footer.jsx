2c9e25e587eb62ac64e040783bbcc545b169da95
anashadfi1 <anvshf@gmail.com>
2026-02-17 21:20:43 +0000

    adding the footer section

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,14 +8,13 @@ const Footer = () => {
             <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2'/>
             anashadfi1@gmail.com
         </div>
-        <div className='text-center sm:flex items-center justify-between border-transparent border-gray-400 mx-[10%] mt-12 py-6'>
+        <div className='text-center sm:flex items-center justify-between border-t
+                        border-gray-400 mx-[10%] mt-12 py-6'>
             <p>&copy;Anas Hadfi. All rights reserverd</p>
         <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-            <li><a href="github">Github</a></li>
-            <li><a href="linkedin">Linkedin</a></li>
-            <li><a href="twitter">Twitter</a></li>
-            <li><a href="kaggle">Kaggle</a></li>
-            <li><a href="kaggle">Hackerrank</a></li>
+            <li><a href="https://github.com/anashadfi1">Github</a></li>
+            <li><a href="https://www.linkedin.com/in/anas-hadfi/">Linkedin</a></li>
+            <li><a href="https://www.hackerrank.com/profile/anashadfi1">Hackerrank</a></li>
         </ul>
         </div>
     </div>
