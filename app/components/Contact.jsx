6684be34e7e65bc5930b1d69b7077608b61af4ff
Anas Hadfi <anvshf@gmail.com>
2026-03-03 04:04:53 +0000

    adding web3form to the contact form

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -4,6 +4,30 @@ import Image from 'next/image'
 import { assets } from '@/assets/assets'
 import { motion } from 'motion/react'
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+  const apiKey = process.env.NEXT_PUBLIC_WEB3FORMS_KEY || "fallback-for-debug"
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", apiKey);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <motion.div 
     id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] 
@@ -27,13 +51,16 @@ const Contact = () => {
                   transition={{delay:0.7,duration:0.5}}>
         I would love to hear from you! If you have any questions, comments, or feedback, please use the form below.
         </motion.p>
-        <motion.form className='max-w-2xl mx-auto'
+        <motion.form onSubmit={onSubmit}  className='max-w-2xl mx-auto'
                   initial={{opacity:0}}
                   whileInView={{opacity:1}}
                   transition={{delay:0.9,duration:0.5}}>
             <div className='grid grid-cols-auto gap-6 mt-10 mb-1'>
               <div className='flex'>
-                <motion.input type="text" 
+                
+                <motion.input 
+                type="text" 
+                name="name" 
                 required
                 className='flex-1 p-3 outline-none border-[0.5px] 
                             border-gray-400 rounded-md bg-white mr-2'
@@ -41,7 +68,10 @@ const Contact = () => {
                 initial={{y:20, opacity:0}}
                 whileInView={{y:0, opacity:1}}
                 transition={{duration:0.6, delay:1.1}}/>
-                <motion.input type="email" placeholder='Enter your e-mail' 
+                <motion.input 
+                type="email"
+                name="email" 
+                placeholder='Enter your e-mail' 
                 required
                 className='flex-1 p-3 outline-none border-[0.5px] 
                             border-gray-400 rounded-md bg-white ml-2'
@@ -50,7 +80,8 @@ const Contact = () => {
                 transition={{duration:0.6, delay:1.2}}/>
               </div>
             </div>
-            <motion.textarea rows="6"  
+            <motion.textarea rows="6"
+            name="message"  
             placeholder='Enter your message' 
             required
             className="w-full p-4 outline-none border-[0.5px] border-gray-400
@@ -67,6 +98,7 @@ const Contact = () => {
                           transition={{duration:0.3}}
                           >Submit now<Image src={assets.right_arrow_white} alt='' className='w-4'/></motion.button>
         </motion.form>
+        <p className='mt-4'>{result}</p>
     </motion.div>
   )
 }
